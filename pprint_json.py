11ec4a4c3c3bd51491e94a7d236909c594c0a1d0
Artem Aleksashkin <aaleksashkin@iponweb.net>
2017-07-27 17:43:04 +0300

    pretty print code is done

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,24 @@
+import sys
 import json
 
 
 def load_data(filepath):
-    pass
+    file_descriptor = open(filepath, "r")
+    file_contents = file_descriptor.read()
+    file_descriptor.close()
+    return file_contents
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(json.loads(data), sort_keys=True, indent = 4, encoding="utf-8", ensure_ascii=False, separators = (',', ': ')))
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2 or sys.argv[1] == '--help':
+        print('JSON Pretty Print')
+        print('Usage python pprint_json.py filename')
+        sys.exit(0)
+
+    data = load_data(sys.argv[1])
+    pretty_print_json(data)
+    sys.exit(0)
